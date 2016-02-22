a20be1cb75897c8c13ae53bf589578041d56a3c6
xzx <xzx@exoticcube.com>
2016-02-22 19:56:03 +0800

    短信会分段，做了拼接

--- app/src/main/java/com/name/myassistant/shortMessage/SmsReceiver.java
+++ app/src/main/java/com/name/myassistant/shortMessage/SmsReceiver.java
@@ -19,19 +19,20 @@ public class SmsReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Bundle bundle = intent.getExtras();
         SmsMessage msg;
+        String info="";
         if (null != bundle) {
             Object[] smsObj = (Object[]) bundle.get("pdus");
             for (Object object : smsObj) {
                 msg = SmsMessage.createFromPdu((byte[]) object);
-
-                Intent intent1=new Intent(context, MainActivity.class);
-                intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent1.putExtra("info",msg.getDisplayMessageBody());
-                context.startActivity(intent1);
+                info+=msg.getDisplayMessageBody();
                 LogUtil.d("xzx", "number:" + msg.getOriginatingAddress()
                         + "   body:" + msg.getDisplayMessageBody() + "  time:"
                         + msg.getTimestampMillis());
             }
+            Intent intent1=new Intent(context, MainActivity.class);
+            intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent1.putExtra("info",info);
+            context.startActivity(intent1);
         }
     }
 }
